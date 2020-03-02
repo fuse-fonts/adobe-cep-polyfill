95a861b3e38f14b46eee390d6ab12f62de065c29
Rory Duncan <robust.rory@gmail.com>
2020-03-01 16:06:06 -0800

    Add first test

---
+++ index.test.js
@@ -0,0 +1,15 @@
+const polyfill = require("./index.testable.js");
+
+describe("__adobe_cep__", () => {
+
+  const polyfilled = polyfill();
+
+  test("is added to window as window.__adobe_cep__", () => {
+    expect(window.__adobe_cep__).not.toBe(undefined);
+  });
+})
+
+
+
+
+
